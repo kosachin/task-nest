c4b940fae845cd7dfb7d5e45eb892c2ec1aa62a0
kosachin <kosachin313@gmail.com>
2025-08-30 15:55:03 +0530

    minor fix

--- libs/common/src/database/database.module.ts
+++ libs/common/src/database/database.module.ts
@@ -27,7 +27,9 @@ import { TypeOrmModule } from '@nestjs/typeorm';
           database: configService.get<string>('DB_NAME'),
           autoLoadEntities: true,
           synchronize: false,
-          ssl: false,
+          ssl: {
+            rejectUnauthorized: false,
+          },
         };
       },
     }),
